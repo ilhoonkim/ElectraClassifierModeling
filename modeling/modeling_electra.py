19c94724a74cf0de0c266022f0685fb31b8bab81
ilhoonkim <45644085+ilhoonkim@users.noreply.github.com>
2022-05-24 15:56:03 +0900

    Update modeling_electra.py

--- modeling/modeling_electra.py
+++ modeling/modeling_electra.py
@@ -887,23 +887,17 @@ class ElectraClassificationHead(nn.Module):
 
     def __init__(self, config):
         super().__init__()
-        # self.dense = nn.Linear(config.hidden_size, config.hidden_size)
-        self.dense = nn.Linear(config.hidden_size *32 , 768)
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.out_proj = nn.Linear(config.hidden_size, config.num_labels)
 
     def forward(self, features, **kwargs):
-        # x = features[:, 0, :]  # take <s> token (equiv. to [CLS])
-        batch_size = features.size()[0]
-        x = features.view(batch_size,1, -1)
+        x = features[:, 0, :]  # take <s> token (equiv. to [CLS])
         x = self.dropout(x)
         x = self.dense(x)
         x = get_activation("gelu")(x)  # although BERT uses tanh here, it seems Electra authors used gelu here
         x = self.dropout(x)
-        x = x.squeeze(1)
         x = self.out_proj(x)
-        
-        
         return x
 
 
